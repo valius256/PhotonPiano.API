cdc3cf16e23f54afc65a3bd4cbd9006276cccb31
Valius256 <quangphat7a1@gmail.com>
2025-01-21 09:02:15 +0700

    FIx Fix Convention code

--- PhotonPiano.Api/Responses/Room/RoomSimpleResponse.cs
+++ PhotonPiano.Api/Responses/Room/RoomSimpleResponse.cs
@@ -2,7 +2,7 @@ using PhotonPiano.DataAccess.Models.Enum;
 
 namespace PhotonPiano.Api.Responses.Room;
 
-public class RoomSimpleResponse
+public record RoomSimpleResponse
 {
     public Guid Id { get; set; }
     public string? Name { get; set; }

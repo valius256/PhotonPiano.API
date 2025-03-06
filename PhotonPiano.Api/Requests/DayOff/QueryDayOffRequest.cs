cd45bc92fbc30236b5e076f58945bc7b5c234396
kisphapha <thanhhung16082003@gmail.com>
2025-03-06 10:42:17 +0700

    fix  cud studentClass and add dayoff controller

---
+++ PhotonPiano.Api/Requests/DayOff/QueryDayOffRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotonPiano.Api.Requests.Query;
+
+namespace PhotonPiano.Api.Requests.DayOff
+{
+    public record QueryDayOffRequest : QueryPagedRequest
+    {
+        [FromQuery(Name = "name")] public string? Name { get; init; }
+        [FromQuery(Name = "start-time")] public DateTime? StartTime { get; init; }
+        [FromQuery(Name = "end-time")] public DateTime? EndTime { get; init; }
+    }
+}
---
+++ PhotonPiano.BusinessLogic/Services/DayOffService.cs
@@ -0,0 +1,46 @@
+﻿
+using PhotonPiano.BusinessLogic.BusinessModel.DayOff;
+using PhotonPiano.BusinessLogic.BusinessModel.EntranceTest;
+using PhotonPiano.BusinessLogic.Interfaces;
+using PhotonPiano.DataAccess.Abstractions;
+using PhotonPiano.DataAccess.Models.Paging;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhotonPiano.BusinessLogic.Services
+{
+    public class DayOffService : IDayOffService
+    {
+        private readonly IServiceFactory _serviceFactory;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DayOffService(IUnitOfWork unitOfWork, IServiceFactory serviceFactory)
+        {
+            _unitOfWork = unitOfWork;
+            _serviceFactory = serviceFactory;
+        }
+        public async Task<PagedResult<DayOffModel>> GetPagedDayOffs(QueryDayOffModel queryDayOff)
+        {
+            var likeKeyword = queryDayOff.GetLikeKeyword();
+
+            var startTime = queryDayOff.StartTime != null ? DateTime.SpecifyKind(queryDayOff.StartTime.Value, DateTimeKind.Utc) : (DateTime?)null;
+            var endTime = queryDayOff.EndTime != null ? DateTime.SpecifyKind(queryDayOff.EndTime.Value, DateTimeKind.Utc) : (DateTime?)null;
+
+            var result = await _unitOfWork.DayOffRepository
+                .GetPaginatedWithProjectionAsync<DayOffModel>(
+                    queryDayOff.Page, queryDayOff.PageSize, queryDayOff.SortColumn, queryDayOff.OrderByDesc,
+                    expressions:
+                    [
+                        d => !startTime.HasValue || d.StartTime >= startTime,
+                        d => !endTime.HasValue || d.StartTime >= endTime,
+                        // search 
+                        d => string.IsNullOrEmpty(queryDayOff.Name) ||
+                             EF.Functions.ILike(EF.Functions.Unaccent(d.Name ?? ""), likeKeyword)
+                        ]);
+
+            // await _serviceFactory.RedisCacheService
+            //     .SaveAsync($"entranceTests_page{query.Page}_pageSize{query.PageSize}_keyword{query.Keyword}",
+            //         result, TimeSpan.FromHours(3));
+            return result;
+        }
+    }
+}

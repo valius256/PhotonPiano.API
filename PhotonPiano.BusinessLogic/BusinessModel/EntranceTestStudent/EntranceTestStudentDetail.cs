e0c4b00ce3641f0f858aea541eb05e36fbbf4b31
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2025-03-14 15:51:37 +0700

    fix-get-entrance-test-student-detail

--- PhotonPiano.BusinessLogic/BusinessModel/EntranceTestStudent/EntranceTestStudentDetail.cs
+++ PhotonPiano.BusinessLogic/BusinessModel/EntranceTestStudent/EntranceTestStudentDetail.cs
@@ -6,7 +6,7 @@ namespace PhotonPiano.BusinessLogic.BusinessModel.EntranceTestStudent;
 
 public record EntranceTestStudentDetail : EntranceTestStudentModel
 {
-    public EntranceTestModel? EntranceTest { get; init; }
+    public EntranceTestWithInstructorModel? EntranceTest { get; init; }
     public AccountSimpleModel? Student { get; init; }
     public List<EntranceTestResultModel> EntranceTestResults { get; set; } = [];
 }

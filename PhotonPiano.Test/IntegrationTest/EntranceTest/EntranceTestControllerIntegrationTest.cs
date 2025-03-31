81ee58a3d47edaa6643639235df005f36aa637a7
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2025-03-31 13:10:01 +0700

    Update some integration test cases for entrance tests module

---
+++ PhotonPiano.Test/IntegrationTest/EntranceTest/EntranceTestControllerIntegrationTest.cs
@@ -0,0 +1,205 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using PhotonPiano.Api.Requests.EntranceTest;
+using PhotonPiano.DataAccess.Models.Enum;
+using PhotonPiano.Test.Extensions;
+
+namespace PhotonPiano.Test.IntegrationTest.EntranceTest;
+
+public class EntranceTestControllerIntegrationTest : BaseIntergrationTest
+{
+    private readonly HttpClient _client;
+
+    private readonly string _baseUrl = "/api/entrance-tests";
+    
+    public EntranceTestControllerIntegrationTest(IntergrationTestWebAppFactory factory) : base(factory)
+    {
+        _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = false
+        });
+    }
+
+    [Fact]
+    public async Task GetEntranceTests_Unauthorized_ReturnsUnauthorized()
+    {
+        //Act
+        var response = await _client.GetAsync($"{_baseUrl}");
+
+        //Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetEntranceTests_GetSuccessAndAuthorized_ReturnsOkStatusWithCorrectHeaderData()
+    {
+        //Arrange
+        var token = await _client.GetAuthToken("staff123@gmail.com", "Password1@");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var queryRequest = new QueryEntranceTestRequest
+        {
+            Page = 1,
+            PageSize = 5,
+            SortColumn = "CreatedAt",
+            OrderByDesc = false
+        };
+
+        var url = $"{_baseUrl}/api/entrance-tests";
+        
+        url += $"?page={queryRequest.Page}&size={queryRequest.PageSize}&column={queryRequest.SortColumn}&desc={queryRequest.OrderByDesc}";
+        
+        //Act
+        var response = await _client.GetAsync(url);    
+        
+        var headers = response.Headers;
+        
+        var page = headers.GetValues("X-Page").FirstOrDefault();
+        var pageSize = headers.GetValues("X-Page-Size").FirstOrDefault();
+        var totalCount = headers.GetValues("X-Total-Count").FirstOrDefault();
+        var totalPages = headers.GetValues("X-Total-Pages").FirstOrDefault();
+        
+        //Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(page, page);
+        Assert.Equal(pageSize, pageSize);
+        Assert.NotNull(totalCount);
+        Assert.NotNull(totalPages);
+    }
+    
+    [Fact]
+    public async Task GetEntranceTestDetails_Unauthorized_ReturnsUnauthorizedStatus()
+    {
+        //Arrange
+        var token = await _client.GetAuthToken("staff123@gmail.com", "Password1@");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        
+        //Act 
+        var response = await _client.GetAsync($"{_baseUrl}/682e3e48-50ca-464e-aea5-1964e3a03811");
+
+        //Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetEntranceTestDetails_EntranceTestNotFound_ReturnsNotFoundStatus()
+    {
+        //Arrange
+        var token = await _client.GetAuthToken("staff123@gmail.com", "Password1@");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var entranceTestId = Guid.NewGuid();
+
+        //Act 
+        var response = await _client.GetAsync($"{_baseUrl}/{entranceTestId}");
+
+        //Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetEntranceTestDetails_GetSuccessAndAuthorized_ReturnsOkStatus()
+    {
+        //Arrange
+        var token = await _client.GetAuthToken("staff123@gmail.com", "Password1@");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        
+        var entranceTestId = "682e3e48-50ca-464e-aea5-1964e3a03811";
+        
+        //Act
+        var response = await _client.GetAsync($"{_baseUrl}/{entranceTestId}");
+        
+        //Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task CreateEntranceTest_Unauthorized_ReturnsUnauthorizedStatus()
+    {
+        //Arrange
+        var createRequest = new CreateEntranceTestRequest
+        {
+            Name = "Test Entrance Test",
+            RoomId = Guid.NewGuid(),
+        };
+        
+        string json = JsonConvert.SerializeObject(createRequest);
+        HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //Act
+        var response = await _client.PostAsync($"{_baseUrl}", content);
+
+        //Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task CreateEntranceTest_CreateSuccess_ReturnsCreatedStatus()
+    {
+        //Arrange
+        var token = await _client.GetAuthToken("staff123@gmail.com", "Password1@");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var createRequest = new CreateEntranceTestRequest
+        {
+            Name = "Test Entrance Test",
+            RoomId = Guid.NewGuid(),
+            Date = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(3)),
+            Shift = Shift.Shift3_10h45_12h,
+            InstructorId = "1axRN4fG0ybZyDOYvO8wnkm5lHJ3",
+            IsAnnouncedScore = false
+        };
+        
+        string json = JsonConvert.SerializeObject(createRequest);
+        HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //Act
+        var response = await _client.PostAsync($"{_baseUrl}", content);
+
+        //Assert
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task DeleteEntranceTest_Unauthorized_ReturnsUnauthorizedStatus()
+    {
+        //Act
+        var response = await _client.DeleteAsync($"{_baseUrl}/{Guid.NewGuid()}");
+        
+        //Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task DeleteEntranceTest_AuthorizedAndEntranceTestNotFound_ReturnsNotFoundStatus()
+    {
+        //Arrange
+        var token = await _client.GetAuthToken("staff123@gmail.com", "Password1@");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        
+        var entranceTestId = Guid.NewGuid();
+        
+        //Act
+        var response = await _client.DeleteAsync($"{_baseUrl}/{entranceTestId}");
+        
+        //Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task DeleteEntranceTest_AuthorizedAndDeleteSuccess_ReturnsNoContentStatus()
+    {
+        //Arrange
+        var token = await _client.GetAuthToken("staff123@gmail.com", "Password1@");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        
+        var entranceTestId = "682e3e48-50ca-464e-aea5-1964e3a03811";
+        
+        //Act
+        var response = await _client.DeleteAsync($"{_baseUrl}/{entranceTestId}");
+        
+        //Assert
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+}

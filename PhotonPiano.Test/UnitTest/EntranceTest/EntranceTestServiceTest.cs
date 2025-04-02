d3e39c0386849826d668cd40f29841a7fb3d68a4
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2025-04-02 19:31:40 +0700

    Update more unit test cases for entrance test

--- PhotonPiano.Test/UnitTest/EntranceTest/EntranceTestServiceTest.cs
+++ PhotonPiano.Test/UnitTest/EntranceTest/EntranceTestServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using AutoFixture;
+﻿using System.Linq.Expressions;
+using AutoFixture;
 using AutoFixture.AutoMoq;
 using Moq;
 using PhotonPiano.BusinessLogic.BusinessModel.Account;
@@ -6,13 +7,16 @@ using PhotonPiano.BusinessLogic.BusinessModel.Class;
 using PhotonPiano.BusinessLogic.BusinessModel.EntranceTest;
 using PhotonPiano.BusinessLogic.BusinessModel.EntranceTestStudent;
 using PhotonPiano.BusinessLogic.BusinessModel.Level;
+using PhotonPiano.BusinessLogic.BusinessModel.Payment;
 using PhotonPiano.BusinessLogic.BusinessModel.Room;
+using PhotonPiano.BusinessLogic.BusinessModel.SystemConfig;
 using PhotonPiano.BusinessLogic.Interfaces;
 using PhotonPiano.BusinessLogic.Services;
 using PhotonPiano.DataAccess.Abstractions;
 using PhotonPiano.DataAccess.Models.Entity;
 using PhotonPiano.DataAccess.Models.Enum;
 using PhotonPiano.Shared.Exceptions;
+using PhotonPiano.Shared.Utils;
 
 namespace PhotonPiano.Test.UnitTest.EntranceTest;
 
@@ -27,14 +31,28 @@ public class EntranceTestServiceTest
     private readonly Mock<IEntranceTestRepository> _entranceTestRepositoryMock;
     private readonly Mock<IRoomRepository> _roomRepositoryMock;
     private readonly Mock<IEntranceTestStudentRepository> _entranceTestStudentRepositoryMock;
+    private readonly Mock<ITransactionRepository> _transactionRepositoryMock;
+    private readonly Mock<IAccountRepository> _accountRepositoryMock;
 
     private readonly Mock<IRoomService> _roomServiceMock;
     private readonly Mock<IAccountService> _accountServiceMock;
+    private readonly Mock<ISystemConfigService> _systemConfigServiceMock;
 
     private readonly IEntranceTestService _entranceTestService;
 
     private readonly AccountModel _sampleStaff;
     private readonly AccountModel _sampleStudent;
+
+    private readonly List<SystemConfigModel> _systemConfigs =
+    [
+        new SystemConfigModel
+        {
+            Id = Guid.NewGuid(),
+            ConfigName = ConfigNames.AllowEntranceTestRegistering,
+            ConfigValue = "false",
+            Role = Role.Administrator
+        }
+    ];
 
     public EntranceTestServiceTest()
     {
@@ -58,14 +76,22 @@ public class EntranceTestServiceTest
         _entranceTestRepositoryMock = _fixture.Freeze<Mock<IEntranceTestRepository>>();
         _roomRepositoryMock = _fixture.Freeze<Mock<IRoomRepository>>();
         _entranceTestStudentRepositoryMock = _fixture.Freeze<Mock<IEntranceTestStudentRepository>>();
+        _transactionRepositoryMock = _fixture.Freeze<Mock<ITransactionRepository>>();
+        _accountRepositoryMock = _fixture.Freeze<Mock<IAccountRepository>>();
 
         _roomServiceMock = _fixture.Freeze<Mock<IRoomService>>();
         _accountServiceMock = _fixture.Freeze<Mock<IAccountService>>();
+        _systemConfigServiceMock = _fixture.Freeze<Mock<ISystemConfigService>>();
 
         _unitOfWorkMock.Setup(uow => uow.EntranceTestRepository).Returns(_entranceTestRepositoryMock.Object);
         _unitOfWorkMock.Setup(uow => uow.RoomRepository).Returns(_roomRepositoryMock.Object);
         _unitOfWorkMock.Setup(uow => uow.EntranceTestStudentRepository)
             .Returns(_entranceTestStudentRepositoryMock.Object);
+        _unitOfWorkMock.Setup(uow => uow.TransactionRepository).Returns(_transactionRepositoryMock.Object);
+        _unitOfWorkMock.Setup(uow => uow.AccountRepository).Returns(_accountRepositoryMock.Object);
+
+        _serviceFactoryMock.Setup(sf => sf.RoomService).Returns(_roomServiceMock.Object);
+        _serviceFactoryMock.Setup(sf => sf.SystemConfigService).Returns(_systemConfigServiceMock.Object);
 
         _entranceTestService = new EntranceTestService(_unitOfWorkMock.Object, _serviceFactoryMock.Object);
     }
@@ -111,6 +137,107 @@ public class EntranceTestServiceTest
         Assert.IsType<ForbiddenMethodException>(result);
         Assert.Equal("You are not allowed to view this entrance test information.", result.Message);
     }
+
+    [Fact]
+    public async Task EnrollEntranceTest_EntranceTestRegisteringIsClosed_ThrowsBadRequestException()
+    {
+        //Arrange
+
+        _systemConfigServiceMock.Setup(service => service.GetAllEntranceTestConfigs()).ReturnsAsync(_systemConfigs);
+
+        //Act
+        var record =
+            await Record.ExceptionAsync(() => _entranceTestService.EnrollEntranceTest(_sampleStudent, "", "", ""));
+
+        //Assert
+        Assert.IsType<BadRequestException>(record);
+        Assert.Equal("Entrance test registering is closed for now.", record.Message);
+    }
+
+    [Fact]
+    public async Task EnrollEntranceTest_StudentIsNotValidToEnroll_ThrowsBadRequestException()
+    {
+        //Arrange
+        _systemConfigServiceMock.Setup(service => service.GetAllEntranceTestConfigs()).ReturnsAsync([
+            new SystemConfigModel
+            {
+                Id = Guid.NewGuid(),
+                ConfigName = ConfigNames.AllowEntranceTestRegistering,
+                ConfigValue = "true",
+                Role = Role.Administrator
+            }
+        ]);
+
+        //Act
+        var record =
+            await Record.ExceptionAsync(() => _entranceTestService.EnrollEntranceTest(_sampleStudent with
+            {
+                StudentStatus = StudentStatus.AttemptingEntranceTest
+            }, "", "", ""));
+
+        //Assert
+        Assert.IsType<BadRequestException>(record);
+        Assert.Equal(
+            "Student is must be in DropOut or Unregistered in order to be accepted to enroll in entrance test.",
+            record.Message);
+    }
+
+    [Fact]
+    public async Task HandleEnrollmentPaymentCallback_TransactionNotFound_ThrowsPaymentRequiredException()
+    {
+        //Arrange
+        var transactionCode = Guid.NewGuid();
+        var callbackModel = _fixture.Build<VnPayCallbackModel>()
+            .With(x => x.VnpTxnRef, transactionCode.ToString())
+            .Create();
+
+        _transactionRepositoryMock.Setup(repo => repo.FindSingleAsync(t => t.Id == transactionCode, true, false))
+            .ReturnsAsync((Transaction?)null);
+
+        //Act
+        var record = await Record.ExceptionAsync(() =>
+            _entranceTestService.HandleEnrollmentPaymentCallback(callbackModel, _sampleStudent.AccountFirebaseId));
+
+        //Assert
+        Assert.IsType<PaymentRequiredException>(record);
+        Assert.Equal("Payment is required", record.Message);
+    }
+
+    [Fact]
+    public async Task HandleEnrollmentPaymentCallback_TransactionAccountNotFound_ThrowsNotFoundException()
+    {
+        // Arrange
+        var transactionCode = Guid.NewGuid();
+        var callbackModel = _fixture.Build<VnPayCallbackModel>()
+            .With(x => x.VnpTxnRef, transactionCode.ToString())
+            .Create();
+
+        _transactionRepositoryMock.Reset(); // Ensure previous setups don't interfere
+
+        _transactionRepositoryMock.Setup(repo => repo.FindSingleAsync(t => t.Id == transactionCode, true, false))
+            .ReturnsAsync(new Transaction
+            {
+                Id = transactionCode, // Make sure the transaction ID matches
+                CreatedById = "",
+                CreatedAt = DateTime.UtcNow,
+                CreatedByEmail = ""
+            });
+
+        _accountRepositoryMock.Reset(); // Reset mock to avoid conflicts from other tests
+
+        _accountRepositoryMock.Setup(repo =>
+                repo.FindSingleAsync(It.IsAny<Expression<Func<Account, bool>>>(), true, false))
+            .ReturnsAsync((Account?)null);
+
+        // Act
+        var record = await Record.ExceptionAsync(() =>
+            _entranceTestService.HandleEnrollmentPaymentCallback(callbackModel, _sampleStudent.AccountFirebaseId));
+
+        // Assert
+        Assert.IsType<NotFoundException>(record);
+        Assert.Equal("Account not found", record.Message);
+    }
+
 
     [Fact]
     public async Task CreateEntranceTest_RoleIsNotInstructor_ThrowsBadRequestException()
@@ -176,14 +303,14 @@ public class EntranceTestServiceTest
     {
         //Arrange
         var entranceTestId = Guid.NewGuid();
-
+    
         var roomId = Guid.NewGuid();
-
+    
         var updateModel = _fixture.Build<UpdateEntranceTestModel>()
             .With(x => x.RoomId, roomId)
             .With(x => x.StartTime, DateOnly.FromDateTime(DateTime.UtcNow))
             .Create();
-
+    
         _entranceTestRepositoryMock.Setup(repo => repo.FindSingleAsync(e => e.Id == entranceTestId, true, false))
             .ReturnsAsync(new DataAccess.Models.Entity.EntranceTest
             {
@@ -191,17 +318,18 @@ public class EntranceTestServiceTest
                 Date = DateOnly.FromDateTime(DateTime.UtcNow),
                 Name = "test",
                 Shift = Shift.Shift1_7h_8h30,
-                RoomId = Guid.NewGuid(),
+                RoomId = roomId,
                 CreatedById = Guid.NewGuid().ToString()
             });
-
-        _roomRepositoryMock.Setup(repo => repo.FindSingleAsync(r => r.Id == roomId, false, false))
+        
+    
+        _roomRepositoryMock.Setup(repo => repo.FindSingleAsync(It.IsAny<Expression<Func<Room, bool>>>(), false, false))
             .ReturnsAsync((Room?)null);
-
+    
         //Act
         var record = await Record.ExceptionAsync(() => _entranceTestService.UpdateEntranceTest(entranceTestId,
             updateModel, _sampleStaff.AccountFirebaseId));
-
+    
         //Assert
         Assert.IsType<NotFoundException>(record);
         Assert.Equal("Room not found.", record.Message);
@@ -249,8 +377,8 @@ public class EntranceTestServiceTest
                 .ToList();
 
         _entranceTestStudentRepositoryMock.Setup(repo =>
-            repo.FindProjectedAsync<EntranceTestStudentWithResultsModel>(
-                x => x.EntranceTestId == entranceTestId, false, false, TrackingOption.Default))
+                repo.FindProjectedAsync<EntranceTestStudentWithResultsModel>(
+                    x => x.EntranceTestId == entranceTestId, false, false, TrackingOption.Default))
             .ReturnsAsync(entranceTestStudents);
 
         //Act

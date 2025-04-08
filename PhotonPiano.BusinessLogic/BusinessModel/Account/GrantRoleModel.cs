ce0b356df0a5295074934ad72372a54881374b36
kisphapha <thanhhung16082003@gmail.com>
2025-04-09 00:57:01 +0700

    add new staff, grant role

---
+++ PhotonPiano.BusinessLogic/BusinessModel/Account/GrantRoleModel.cs
@@ -0,0 +1,12 @@
+﻿
+
+using PhotonPiano.DataAccess.Models.Enum;
+
+namespace PhotonPiano.BusinessLogic.BusinessModel.Account
+{
+    public record GrantRoleModel
+    {
+        public required string AccountFirebaseId { get; init; }
+        public Role Role { get; init; }
+    }
+}

06f32382eef9ce19d9409a10f0b4a1388ad7c7a0
Valius256 <quangphat7a1@gmail.com>
2025-04-14 18:35:22 +0700

    feat: add tuition creation and deadline notification to class registration process

---
+++ PhotonPiano.DataAccess/Migrations/20250414112450_Update_Value_DeadlinePayment_In_Table_SystemConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhotonPiano.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Value_DeadlinePayment_In_Table_SystemConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""SystemConfig""
+                SET ""ConfigValue"" = '4',
+                    ""UpdatedAt"" = CURRENT_TIMESTAMP
+                WHERE ""ConfigName"" = 'Hạn chót thanh toán học phí'
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

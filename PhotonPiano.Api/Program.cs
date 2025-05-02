63cdef4da3a04e45d3913f213fb7b1e3feb83684
Quang Phat <68989339+valius256@users.noreply.github.com>
2025-05-02 17:06:00 +0700

    feat: enhance Prometheus configuration in deploy.yml to monitor VPS s… (#263)
    * feat: enhance Prometheus configuration in deploy.yml to monitor VPS system

--- PhotonPiano.Api/Program.cs
+++ PhotonPiano.Api/Program.cs
@@ -1,18 +1,18 @@
+using System.Reflection;
 using DinkToPdf;
 using DinkToPdf.Contracts;
+using Ghostscript.NET.Rasterizer;
 using Hangfire;
 using Mapster;
 using Microsoft.AspNetCore.Mvc.Razor;
+using OpenTelemetry.Metrics;
 using PhotonPiano.Api.Configurations;
 using PhotonPiano.Api.Extensions;
 using PhotonPiano.BusinessLogic.Extensions;
 using PhotonPiano.DataAccess.Extensions;
 using PhotonPiano.PubSub;
-using Serilog;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using Ghostscript.NET.Rasterizer;
 using Prometheus;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHostedService<DbMigrationJob>();
@@ -23,11 +23,29 @@ var configuration = builder.Configuration;
 // hello this line write to proved i am the owner of this project and github is not good for beginner
 
 
-
 // Add services to the container.
 builder.Services.AddApiDependencies(configuration)
     .AddBusinessLogicDependencies()
-    .AddDataAccessDependencies(); 
+    .AddDataAccessDependencies();
+
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+            "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new[]
+                {
+                    0, 0.005, 0.01, 0.025, 0.05,
+                    0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
+                }
+            });
+    });
 
 // var wkhtmltoxPath = Path.Combine(Directory.GetCurrentDirectory(), "wkhtmltox", "v0.12.6");
 // var context = new CustomAssemblyLoadContext();
@@ -40,7 +58,8 @@ builder.Services.AddApiDependencies(configuration)
 builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
 
 // Register Ghostscript.NET services
-builder.Services.AddSingleton<GhostscriptRasterizer>(provider => {
+builder.Services.AddSingleton<GhostscriptRasterizer>(provider =>
+{
     // You can optionally set a specific Ghostscript version path here if needed:
     // var gsPath = Path.Combine(Directory.GetCurrentDirectory(), "ghostscript", "gsdll32.dll");
     // GhostscriptVersionInfo gsVersion = new GhostscriptVersionInfo(gsPath);
@@ -78,6 +97,7 @@ builder.AddSignalRConfig();
 
 builder.Services.AddSingleton<RedirectUrlValidator>();
 
+
 //Add serilog
 builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 //builder.AddServiceDefaults();
@@ -88,17 +108,21 @@ TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
-app.UseMetricServer(); // expose Prometheus metrics at /metrics
-app.UseHttpMetrics(); // collect HTTP request metrics
+
+// app.UseMetricServer(); // expose Prometheus metrics at /metrics
+// app.UseHttpMetrics(); // collect HTTP request metrics
 
 
 app.UseRouting();
+
 
 await app.ConfigureDatabaseAsync();
 
 app.UseScalarConfig();
 
 app.UseCors("AllowAll");
+
+// app.MapPrometheusScrapingEndpoint();
 
 app.UseHttpsRedirection();
 
@@ -108,7 +132,7 @@ app.UseHangfireDashboard("/hangfire", new DashboardOptions
     DarkModeEnabled = true,
     IsReadOnlyFunc = _ => false,
     Authorization = new[] { new HangfireAuthorizationFilter() },
-    AppPath = "https://photonpiano.duckdns.org/scalar/v1",
+    AppPath = "https://photonpiano.duckdns.org/scalar/v1"
 });
 
 app.UseStaticFiles();
@@ -127,10 +151,12 @@ app.UseAuthorization();
 
 app.MapSignalRConfig();
 
+
 app.UseResponseCompression();
 
 app.MapControllers();
 
+app.MapPrometheusScrapingEndpoint();
 
 app.MapHealthChecks("/health");
 

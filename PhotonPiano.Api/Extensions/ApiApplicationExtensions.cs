81707a31d266bfa335954cd503cf031a89e581f2
Valius256 <quangphat7a1@gmail.com>
2025-05-09 09:41:27 +0700

    feat: enhance Scalar API configuration with updated theme and base server URL

--- PhotonPiano.Api/Extensions/ApiApplicationExtensions.cs
+++ PhotonPiano.Api/Extensions/ApiApplicationExtensions.cs
@@ -5,15 +5,21 @@ public static class ApiApplicationExtensions
     public static WebApplication UseScalarConfig(this WebApplication app)
     {
         app.MapOpenApi();
+
         app.MapScalarApiReference(options =>
         {
             options.WithTitle("PhotonPiano API");
 
+            options.Theme = ScalarTheme.Purple;
+
+            options.BaseServerUrl = "https://photonpiano.duckdns.org/scalar/v1";
+            
             options.Authentication =
                 new ScalarAuthenticationOptions
                 {
                     PreferredSecurityScheme = "Bearer"
                 };
+
 
             options.DefaultHttpClient =
                 new KeyValuePair<ScalarTarget, ScalarClient>(ScalarTarget.JavaScript, ScalarClient.Axios);

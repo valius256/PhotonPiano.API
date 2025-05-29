b5e52f0495efef9520d273c0db52f023368a1dc6
kisphapha <thanhhung16082003@gmail.com>
2025-05-29 22:20:23 +0700

    extend class detail

--- PhotonPiano.BusinessLogic/BusinessModel/Class/ClassDetailModel.cs
+++ PhotonPiano.BusinessLogic/BusinessModel/Class/ClassDetailModel.cs
@@ -6,7 +6,7 @@ namespace PhotonPiano.BusinessLogic.BusinessModel.Class
     {
         public decimal PricePerSlots { get; set; }
         public int SlotsPerWeek { get; set; }
-        public ICollection<StudentClassModel> StudentClasses { get; set; } = new List<StudentClassModel>();
+        public ICollection<StudentClassWithClassAndTuitionModel> StudentClasses { get; set; } = new List<StudentClassWithClassAndTuitionModel>();
         public ICollection<SlotModel> Slots { get; set; } = new List<SlotModel>();
     }
 }
